266aa8e12413a8dd17f15d8ae5cd64fa66e87218
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-05-10 09:49:00 +0300

    add home view & init states

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
-const Color kPrimaryColor = Color(0XFF100B20);
+const kPrimaryColor = Color(0XFF100B20);
+const kTransitionDuration = Duration(seconds: 2);
--- lib/feature/widgets/splash_view_body.dart
+++ lib/feature/widgets/splash_view_body.dart
@@ -1,8 +1,11 @@
 import 'package:bookly_app/assets.dart';
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/feature/home/presentation/view/home_view.dart';
 import 'package:bookly_app/feature/widgets/sliding_animation_builder.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -19,16 +22,8 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 6), end: Offset.zero)
-            .animate(animationController);
-
-    animationController.forward();
+    initSlidingAnimation();
+    navigateToHomeView();
   }
 
   @override
@@ -47,5 +42,28 @@ class _SplashViewBodyState extends State<SplashViewBody>
         SlidingAnimationBuilder(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 6), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
+  }
+
+  void navigateToHomeView() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(
+        const HomeView(),
+        transition: Transition.fade,
+        duration: kTransitionDuration,
+      );
+    });
   }
 }
