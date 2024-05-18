d9b0649732d2ce06772bf9517972de2e4e4a8265
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-05-18 18:50:30 +0300

    add errors abstract class

--- lib/feature/home/data/repos/home_repos.dart
+++ lib/feature/home/data/repos/home_repos.dart
@@ -1,6 +1,8 @@
+import 'package:bookly_app/core/errors/errors.dart';
 import 'package:bookly_app/feature/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepos {
-  Future<List<BookModel>> fetchFeaturedBooks();
-  Future<List<BookModel>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
 }
