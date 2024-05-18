7a80de1b30eec52d55f53ba6ce9d9546b9dbc418
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-05-18 18:48:47 +0300

    add dartz to app

--- lib/feature/home/data/repos/home_repos.dart
+++ lib/feature/home/data/repos/home_repos.dart
@@ -1,4 +1,6 @@
+import 'package:bookly_app/feature/home/data/models/book_model/book_model.dart';
+
 abstract class HomeRepos {
-  fetchFeaturedBooks();
-  fetchBestSellerBooks();
+  Future<List<BookModel>> fetchFeaturedBooks();
+  Future<List<BookModel>> fetchBestSellerBooks();
 }
