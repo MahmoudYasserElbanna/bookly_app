30ef0ee509d2ed9bb8c86171fab41bf6eaf136f0
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-05-25 12:15:45 +0300

    refactor search view code

--- lib/feature/search/presentation/view/widgets/search_view_body.dart
+++ lib/feature/search/presentation/view/widgets/search_view_body.dart
@@ -1,11 +1,8 @@
 import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/feature/search/presentation/manager/cubit/search_result_cubit.dart';
 import 'package:bookly_app/feature/search/presentation/view/widgets/custom_search_text_field.dart';
 import 'package:bookly_app/feature/search/presentation/view/widgets/search_view_list_view.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SearchViewBody extends StatefulWidget {
   const SearchViewBody({super.key});
